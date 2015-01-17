a851b8936626e91cc6576056d6d8cb865b7a9d41
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-17 12:36:07 -0500

    add documentation

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,6 +1,8 @@
 /**
  * Created by devin on 1/17/15.
  */
+// this template will return the domain (hostname) given a url and will allow
+// the {{domain}} in posts_list.html to be populated
 Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
